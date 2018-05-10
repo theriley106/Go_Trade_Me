56a16651c12534c7085426d4f0bda5e5f08f0bb0
Theriley106 <christopherlambert106@gmail.com>
2018-05-10 19:36:59 -0400

    Added alexa part

--- main.go
+++ main.go
@@ -21,6 +21,8 @@ import (
 	// Implements the ability to use regex
 	// "time"
 	// This allows you to interact with datetime
+	"github.com/arienmalec/alexa-go"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 type apiStruct struct {
@@ -112,7 +114,50 @@ func generateResponse(tickerVal string, priceVal string) string {
 	return responseVal
 }
 
+
+////  This is all alexa specific stuff
+
+func DispatchIntents(request alexa.Request) alexa.Response {
+	var response alexa.Response
+	switch request.Body.Intent.Name {
+	case "hello":
+		// This is the intent name
+		response = handleHello(request)
+	case alexa.HelpIntent:
+		response = handleHelp()
+	}
+
+	return response
+}
+
+func handleHello(request alexa.Request) alexa.Response {
+	title := "Saying Hello"
+	var text string
+	switch request.Body.Locale {
+	case alexa.LocaleAustralianEnglish:
+		text = "G'day mate!"
+	case alexa.LocaleGerman:
+		text = "Hallo Welt"
+	case alexa.LocaleJapanese:
+		text = "こんにちは世界"
+	default:
+		text = "Hello, World"
+	}
+	return alexa.NewSimpleResponse(title, text)
+}
+
+
+func handleHelp() alexa.Response {
+	return alexa.NewSimpleResponse("Help for Hello", "To receive a greeting, ask hello to say hello")
+}
+
+// Handler is the lambda hander
+func Handler(request alexa.Request) (alexa.Response, error) {
+	return DispatchIntents(request), nil
+}
+
 func main() {
+	lambda.Start(Handler)
 	ticker := "AAPL"
 	// Stock ticker that the price will return
 	valTest := createURL(ticker)
