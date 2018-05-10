edb8eca436d737a485c5b77627e2b319bd6ca266
Theriley106 <christopherlambert106@gmail.com>
2018-05-10 16:13:54 -0400

    Added main function

---
+++ main.go
@@ -0,0 +1,22 @@
+// This is my first Go program - Please don't judge it too harshly xD
+
+package main
+
+
+import (
+	// This is similar to package imports in Python
+	"encoding/json"
+	// Allows you to decode json from the REST api call
+	"fmt"
+	// This allows you to input/output values
+	"log"
+	// Logging info
+	"net/http"
+	// Allows get and post requests to be made
+	"net/url"
+	// THis allows go to parse urls
+)
+
+func main() {
+	return
+}
