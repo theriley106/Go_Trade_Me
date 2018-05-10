29226b6d8cb6b36112197a60bc5c406be2c9d78e
Theriley106 <christopherlambert106@gmail.com>
2018-05-10 16:24:01 -0400

    Added comemnt in README

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Go_Trade_Me
 Alexa Skill made in Go that outputs current NYSE Stock Prices for a given stock ticker
 
+### This is my first Go program - Please don't judge it too harshly xD
 
 ## References
 
--- main.go
+++ main.go
@@ -5,16 +5,21 @@ package main
 
 import (
 	// This is similar to package imports in Python
-	"encoding/json"
+	// "encoding/json"
 	// Allows you to decode json from the REST api call
-	"fmt"
+	// "fmt"
 	// This allows you to input/output values
-	"log"
+	// "log"
 	// Logging info
-	"net/http"
+	// "net/http"
 	// Allows get and post requests to be made
 )
 
+func createURL(tickerVal string) {
+	return ("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + tickerVal + "&interval=1min&apikey=3ZC52BSRXYLK76YY")
+}
+
+
 func main() {
-	return
+	return createURL("AAPL")
 }
